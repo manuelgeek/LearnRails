582cd1ea4ad87e977d996b2fb2138d845ee3ad98
manuelgeek <emashmagak@appslab.co.ke>
2019-08-14 18:35:33 +0300

    Add CSS and import CSS files inside the main file - Create a default.scss file and add CSS - Define  variable - Import default.scss file inside the application.scss

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  devise_scope :user do
+    get 'login', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+  end
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'pages#index'
+end
