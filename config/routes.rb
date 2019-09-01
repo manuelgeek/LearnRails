1d74ba7f882fdae40cad7e32289a73e847fafa52
manuelgeek <emashmagak@appslab.co.ke>
2019-09-01 21:50:20 +0300

    Display posts on the home page - Generate Posts controller and create an index action.   Inside the index action retrieve Post records - Declare routes for posts - Create a _post.html.erb partial inside posts directory - Render posts inside the home page's main content

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,11 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+  resources :posts do
+    collection do
+      get 'hobby'
+      get 'study'
+      get 'team'
+    end
+  end
 end
